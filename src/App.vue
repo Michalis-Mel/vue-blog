60c9542533daf752155f6f100bd67ccf926d9466
Michalis Meliopoulos <mixalismeliop@gmail.com>
2023-09-26 20:01:26 +0300

    added header footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
+  <Header />
   <router-view />
+  <Footer />
 </template>
 
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default { components: { Header, Footer } };
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="row">
+    <router-link to="/" class="logo">
+      <img src="@/assets/logo-small.png" alt="Logo" /> M² BLOG
+    </router-link>
+    <router-link :to="{ name: 'addPost' }" class="create_link"
+      >Create New Post</router-link
+    >
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
