d15be4847110a5e56faac03f72503f023182a7f8
Michalis Meliopoulos <mixalismeliop@gmail.com>
2023-09-27 19:12:22 +0300

    tags functionality fix

--- src/components/CreatePostForm.vue
+++ src/components/CreatePostForm.vue
@@ -12,10 +12,23 @@
     <form @submit.prevent="handleSubmit">
       <label>Title: </label>
       <input v-model="post.title" type="text" required />
+
       <label>Body: </label>
       <input v-model="post.body" type="text" required />
+
       <label>Tags: </label>
-      <input v-model="post.tags" type="text" />
+      <input type="text" v-model="post.tempTag" @keyup="addTag" />
+      <div class="tags">
+        <div
+          v-for="tag in post.tags"
+          :key="tag"
+          class="tag"
+          @click="deleteTag(tag)"
+        >
+          {{ tag }}
+        </div>
+      </div>
+
       <button class="create">Create Post</button>
     </form>
   </div>
@@ -29,13 +42,25 @@ export default {
       post: {
         title: "",
         body: "",
-        tags: "",
+        tempTag: "",
+        tags: [],
       },
       submited: false,
       submitedText: false,
     };
   },
   methods: {
+    addTag(e) {
+      if ((e.key === "," || e.key === " ") && this.post.tempTag) {
+        if (!this.post.tags.includes(this.post.tempTag)) {
+          this.post.tags.push(this.post.tempTag);
+        }
+        this.post.tempTag = "";
+      }
+    },
+    deleteTag(tag) {
+      this.post.tags = this.post.tags.filter((item) => tag !== item);
+    },
     handleSubmit() {
       axios.post("http://localhost:3000/posts", this.post).then((res) => {
         console.log(res);
