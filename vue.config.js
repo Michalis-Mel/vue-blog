23f5928b5987c8468354e3ca3a834d1e241170ff
Michalis Meliopoulos <mixalismeliop@gmail.com>
2023-09-27 19:26:37 +0300

    unpublish

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,3 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
-// module.exports = {
-//   css: {
-//     loaderOptions: {
-//       sass: {
-//         prependData: `
-//           @import "@/scss/_variables.scss";
-//         `,
-//       },
-//     },
-//   },
-// };
